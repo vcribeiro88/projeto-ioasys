8450d6021273c3ef30d764fd5c0975d60faf6fc6
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-01-21 11:33:07 -0400

    fix: correção de bug na segue da home view controller

---
+++ Projeto ioasys/ViewControllers/CompanyDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompanyDetailViewController.swift
+//  Projeto ioasys
+//
+//  Created by Vinícius de Oliveira on 21/01/21.
+//
+
+import UIKit
+
+class CompanyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
