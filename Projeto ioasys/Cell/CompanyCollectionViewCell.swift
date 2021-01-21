ec03c96ed261031defe2edeb383ae89aac671324
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2021-01-21 00:18:23 -0400

    feat: Implementação da collection view

---
+++ Projeto ioasys/Cell/CompanyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CompanyCollectionViewCell.swift
+//  Projeto ioasys
+//
+//  Created by Vinícius de Oliveira on 20/01/21.
+//
+
+import UIKit
+
+class CompanyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var companyLogoImage: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
+}
--- Projeto ioasys/ViewControllers/HomeViewController.swift
+++ Projeto ioasys/ViewControllers/HomeViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var companiesCollectionView: UICollectionView!
+    
+    var companies = ["Empresa X", "Empresa Y", "Empresa Z", "Empresa XZ"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchBar.searchTextField.backgroundColor = UIColor(named: "textFieldBg")
     }
 
+}
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return companies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let companyCell = companiesCollectionView.dequeueReusableCell(withReuseIdentifier: "companyCell", for: indexPath) as! CompanyCollectionViewCell
+        
+        companyCell.layer.cornerRadius = 4
+        companyCell.companyNameLabel.text = companies[indexPath.row]
+        
+        return companyCell
+    }
+    
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = companiesCollectionView.frame.size
+        return CGSize(width: size.width - 32, height: 180)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
+
+extension HomeViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        headerImage.layer.position.y -= 50
+        searchBar.layer.position.y -= 50
+    }
+    
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        <#code#>
+//    }
 }
 
